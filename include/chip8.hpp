c740cc1937dd7288e357d8ecce6f6d4c9da0b946
emxfire <sanskarr6812@gmail.com>
2026-06-13 18:19:58 +0545

    first commit

---
+++ include/chip8.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <cstdint>
+
+struct Chip8 {
+    uint16_t  registers[16];
+    uint8_t   memory[4096];
+    uint16_t  index {}; 
+    uint16_t  pc {};
+    uint16_t  stack[16] {};
+    uint8_t   sp {};
+    uint8_t   delayTimer {};
+    uint8_t   soundTimer {};
+    uint8_t   keypad[16] {};
+    uint32_t  video[64 * 32] {};
+    uint16_t  opcode {};
+};
+
+
+
+
+
